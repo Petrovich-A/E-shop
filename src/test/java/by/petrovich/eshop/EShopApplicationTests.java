a1d9a3ab599480263d1aafe3d719953fe87d8e01
Petrovich-A <A.Piatrovich@gmail.com>
2023-05-12 10:14:57 +0300

    added simple start app

---
+++ src/test/java/by/petrovich/eshop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package by.petrovich.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
