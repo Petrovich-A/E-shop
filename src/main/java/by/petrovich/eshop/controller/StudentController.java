b07385adfa26b48b57ce6cc6c57d077b5db70c50
Petrovich-A <A.Piatrovich@gmail.com>
2023-05-13 10:29:59 +0300

    HTTP Status 404 – Not Found

--- src/main/java/by/petrovich/eshop/controller/StudentController.java
+++ src/main/java/by/petrovich/eshop/controller/StudentController.java
@@ -3,8 +3,7 @@ package by.petrovich.eshop.controller;
 import by.petrovich.eshop.model.Student;
 import by.petrovich.eshop.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-//@RequestMapping
 public class StudentController {
     StudentService studentService;
 
@@ -21,14 +19,16 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @GetMapping("/index")
-    public ResponseEntity<List<Student>> receiveAll(@RequestParam(required = false)String title) {
-        try {
-            List<Student> students = studentService.findAll();
-            return new ResponseEntity<>(students, HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+    @GetMapping("/students")
+    public String findAll(@RequestParam(required = false) Model model) {
+        List<Student> students = studentService.findAll();
+        model.addAttribute("students", students);
+        return "students";
 
+//        try {
+//            List<Student> students = studentService.findAll();
+//            return new ResponseEntity<>(students, HttpStatus.OK);
+//        }catch (Exception e){
+//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
