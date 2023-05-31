b1481491c2e7a51fc7a69bab406535878e399d18
Petrovich-A <A.Piatrovich@gmail.com>
2023-05-31 11:48:32 +0300

    added removing from cart

--- src/main/java/by/petrovich/eshop/model/Cart.java
+++ src/main/java/by/petrovich/eshop/model/Cart.java
@@ -1,23 +1,24 @@
 package by.petrovich.eshop.model;
 
 import by.petrovich.eshop.entity.Product;
-import lombok.Data;
-import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Data
-@Component
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Cart {
     private Map<Integer, Product> products;
-    private double totalPrice = 0;
-
-    public Cart() {
-        this.products = new HashMap<>();
-    }
+    private double totalPrice;
 
     public void addProduct(Product product) {
         products.put(product.getProductId(), product);
