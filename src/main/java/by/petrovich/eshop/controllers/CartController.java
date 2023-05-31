1f540b84c87c5a9e32fd5849de588bfa57869afd
Alexandr <a.piatrovich@gmail.com>
2023-05-31 22:37:23 +0300

    minor fixes

--- src/main/java/by/petrovich/eshop/controllers/CartController.java
+++ src/main/java/by/petrovich/eshop/controllers/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/by/petrovich/eshop/model/Cart.java
+++ src/main/java/by/petrovich/eshop/model/Cart.java
@@ -1,24 +1,23 @@
 package by.petrovich.eshop.model;
 
 import by.petrovich.eshop.entity.Product;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Data
 public class Cart {
     private Map<Integer, Product> products;
     private double totalPrice;
+
+    public Cart() {
+        this.products = new HashMap<>();
+    }
 
     public void addProduct(Product product) {
         products.put(product.getProductId(), product);
