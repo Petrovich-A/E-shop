be004068fa08c4f2eb4376cca19b233e89fa3e73
Alexandr <a.piatrovich@gmail.com>
2023-07-02 18:04:10 +0300

    added logging

--- src/main/java/by/petrovich/eshop/exceptions/GlobalExceptionHandler.java
+++ src/main/java/by/petrovich/eshop/exceptions/GlobalExceptionHandler.java
@@ -1,23 +1,21 @@
 package by.petrovich.eshop.exceptions;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import static by.petrovich.eshop.PageName.ERROR_PAGE;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    private final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(UserNotFoundException.class)
     public ModelAndView handleUserNotFoundException(UserNotFoundException ex) {
         ModelAndView modelAndView = new ModelAndView(ERROR_PAGE);
         modelAndView.addObject("userNotFoundException", ex.getMessage());
-        logger.error("error", ex);
+        log.error("error", ex);
         return modelAndView;
     }
 
