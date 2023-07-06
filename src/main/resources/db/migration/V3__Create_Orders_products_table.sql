296ce242dfc02e07e7f3e4f9a5f7093e79d07a73
Petrovich-A <A.Piatrovich@gmail.com>
2023-07-06 11:11:07 +0300

    fixed bad reference :bug:

--- src/main/resources/db/migration/V3__Create_Orders_products_table.sql
+++ src/main/resources/db/migration/V3__Create_Orders_products_table.sql
@@ -2,7 +2,7 @@
 --
 CREATE TABLE orders_products
 (
-    order_id   INTEGER REFERENCES products (product_id),
-    product_id INTEGER REFERENCES orders (order_id),
+    order_id   INTEGER REFERENCES orders (order_id),
+    product_id INTEGER REFERENCES products (product_id),
     CONSTRAINT orders_products_pk PRIMARY KEY (order_id, product_id)
 );
---
+++ src/main/resources/db/migration/V4__Insert_Orders_products_table.sql
@@ -0,0 +1,12 @@
+-- Inserting in table orders_products
+--
+INSERT INTO orders_products (order_id, product_id)
+VALUES (1, 1),
+       (1, 9),
+       (2, 4),
+       (2, 6),
+       (3, 12),
+       (4, 4),
+       (5, 15),
+       (6, 11),
+       (6, 14);
