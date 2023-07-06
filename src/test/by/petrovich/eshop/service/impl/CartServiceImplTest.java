81e63b7318ae59816c4c68ef0cd3ff0564a0ea61
Petrovich-A <A.Piatrovich@gmail.com>
2023-07-06 11:42:44 +0300

    wrote test

--- src/test/by/petrovich/eshop/service/impl/CartServiceImplTest.java
+++ src/test/by/petrovich/eshop/service/impl/CartServiceImplTest.java
@@ -2,12 +2,15 @@ package by.petrovich.eshop.service.impl;
 
 import by.petrovich.eshop.EShopApplication;
 import by.petrovich.eshop.dto.ProductDto;
+import by.petrovich.eshop.entity.Cart;
+import by.petrovich.eshop.entity.Product;
 import by.petrovich.eshop.service.CartService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,7 +24,7 @@ class CartServiceImplTest {
     }
 
     @Test
-    void calculateTotalPrice() {
+    void testCalculateTotalPrice() {
         double price1 = 10.0;
         double price2 = 5.55;
         ProductDto productDto1 = ProductDto.builder().price(price1).build();
@@ -32,5 +35,17 @@ class CartServiceImplTest {
         double actual = cartService.calculateTotalPrice(productsDto);
         double expected = 15.55;
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAddProduct() {
+        Product product1 = Product.builder().productId(1).name("name1").price(10.5).build();
+        Product product2 = Product.builder().productId(2).name("name2").price(5.0).build();
+        List<Product> products = new ArrayList<>();
+        products.add(product1);
+        products.add(product2);
+        Cart expected = new Cart();
+        expected.setProducts(products);
+
     }
 }
