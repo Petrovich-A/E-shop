9bf9c549054b02555c044e2f667d99c2b11cd7b7
Petrovich-A <A.Piatrovich@gmail.com>
2023-07-11 11:34:59 +0300

    :rotating_light: forgot @Configuration

--- src/main/java/by/petrovich/eshop/config/ConfigBean.java
+++ src/main/java/by/petrovich/eshop/config/ConfigBean.java
@@ -2,7 +2,9 @@ package by.petrovich.eshop.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ConfigBean {
     @Bean
     public ModelMapper modelMapper() {
