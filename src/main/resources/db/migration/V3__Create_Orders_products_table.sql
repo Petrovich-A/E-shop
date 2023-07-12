3c06ee5d2f59583543abe0747e1d8a71bf89df85
Petrovich-A <A.Piatrovich@gmail.com>
2023-07-12 13:41:58 +0300

    :adhesive_bandage: fixed an opportunity to order the same products
    Took 53 minutes

--- src/main/resources/db/migration/V3__Create_Orders_products_table.sql
+++ src/main/resources/db/migration/V3__Create_Orders_products_table.sql
@@ -3,6 +3,5 @@
 CREATE TABLE orders_products
 (
     order_id   INTEGER REFERENCES orders (order_id),
-    product_id INTEGER REFERENCES products (product_id),
-    CONSTRAINT orders_products_pk PRIMARY KEY (order_id, product_id)
+    product_id INTEGER REFERENCES products (product_id)
 );
